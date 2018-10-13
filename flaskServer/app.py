9220ec0633e558f078f08a99468851ba78b6b114
Michael Starkey <2194203s@student.gla.ac.uk>
2018-10-13 23:56:01 +0100

    MY FIRST COMMIT :D :D :D :D

--- flaskServer/app.py
+++ flaskServer/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, jsonify, request
 import urllib.request
+from pymongo import MongoClient
+from bson.code import Code
+from pprint import pprint
+import time
 import json
 import pandas as pd
 
 app = Flask(__name__, static_url_path='/static')
 
 cities_df = pd.read_csv('static/uscitiesv1.4.csv')
+db = MongoClient('localhost', 27017).hackathon
+admissions_coll = db.admissions
+cities_coll = db.cities
 
 @app.route('/')
 def index():
@@ -13,9 +20,11 @@ def index():
 
 @app.route('/getCities', methods=['GET'])
 def getCitites():
-  '''database query logic here'''
-  # Make sure cities are in the format Elkmont, not ELKMONT (plz).
-  cities = ['Huntsville', 'Elkmont', 'Maumelle']
+
+  # takes about 0.03s
+  cities = cities_coll.find().distinct("_id")
+
+  #cities = ['Huntsville', 'Elkmont', 'Maumelle']
   citiesLatLng = {}
   for city in cities:
     latLng = cities_df.loc[cities_df['city'] == city][['lat', 'lng']].iloc[0]
@@ -26,15 +35,16 @@ def getCitites():
 @app.route('/getPostcodes', methods=['POST'])
 def getPostcodes():
   cityName = request.json['cityName']
-  '''database query logic here'''
-  postcodes = ['56763', '89403', '30298']
+  postcodes = admissions_coll.find({"City": cityName}).distinct("Postcode")
+  # postcodes = ['56763', '89403', '30298']
   return jsonify(postcodes)
 
 @app.route('/getPeople', methods=['POST'])
 def getPeople():
   postCode = request.json['postCode']
-  '''database query logic here'''
-  people = ['person1', 'person2', 'person3']
+  cityName = request.json['cityName']
+  people = admissions_coll.find({"City": cityName, "Postcode": postCode}).distinct("AccountNumber")
+  # people = ['person1', 'person2', 'person3']
   return jsonify(people)
 
 @app.route('/getDrg', methods=['POST'])
