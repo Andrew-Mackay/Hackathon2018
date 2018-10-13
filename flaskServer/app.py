c4c97e8e2dec93019d514ec3ac9d32b9825d1c2b
Andrew Mackay <mackay-andrew@hotmail.com>
2018-10-13 12:28:12 +0100

    created api.js for making requests to server

--- flaskServer/app.py
+++ flaskServer/app.py
@@ -2,11 +2,11 @@ from flask import Flask, jsonify, request
 import urllib.request
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
-  return "done"
+  return app.send_static_file('test.html')
 
 @app.route('/getCities', methods=['GET'])
 def getCitites():
@@ -16,21 +16,21 @@ def getCitites():
 
 @app.route('/getPostcodes', methods=['POST'])
 def getPostcodes():
-  cityName = request.form['cityName']
+  cityName = request.json['cityName']
   '''database query logic here'''
   postcodes = ['G127BG', 'AQWFGH', 'FGH423']
   return jsonify(postcodes)
 
 @app.route('/getPeople', methods=['POST'])
 def getPeople():
-  postCode = request.form['postCode']
+  postCode = request.json['postCode']
   '''database query logic here'''
   people = ['person1', 'person2', 'person3']
   return jsonify(people)
 
 @app.route('/getDrg', methods=['POST'])
 def getDrg():
-  drgCode = request.form['drgCode']
+  drgCode = request.json['drgCode']
   drgDescription = translateDrgCode(drgCode)
   return jsonify(drgDescription)
 
