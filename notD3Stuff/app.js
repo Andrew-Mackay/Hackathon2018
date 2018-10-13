d68f6dc9f88499e03dd97cefc312108db411f1d1
Edward Wood <2194801w@student.gla.ac.uk>
2018-10-13 14:33:54 +0100

    🤔 It's all on p5! 🤔

---
+++ notD3Stuff/app.js
@@ -0,0 +1,104 @@
+let renderingPeople = false;
+let direction = -1;
+
+function setup() {
+  createCanvas(720, 400);
+  background(150, 150, 150);
+  for (var i = 0; i < num; i++) {
+    ax[i] = width / 2;
+    ay[i] = height / 2;
+  }
+
+  let data = ["London", "Edinburgh", "Glasgow", "Aberdeen", "York"];
+  renderCities(data);
+}
+
+function createCity(name) {
+  this.x = random(width - 20);
+  this.y = random(height - 20);
+  this.diameter = 20;
+  rect(this.x, this.y, this.diameter, this.diameter);
+  text(name, this.x, this.y);
+  return [x, y];
+}
+
+function renderCities(data) {
+  count = data.length;
+
+  points = {};
+  while (count) {
+    var text = data[count - 1];
+    points[text] = createCity(text);
+    count--;
+  }
+}
+
+function renderPeople() {
+  renderingPeople = true;
+}
+
+var num = 2;
+var range = 4;
+
+var ax = [];
+var ay = [];
+
+function draw() {
+  if (renderingPeople) {
+    background(150, 150, 150);
+
+    // Shift all elements 1 place to the left
+    for (var i = 1; i < num; i++) {
+      ax[i - 1] = ax[i];
+      ay[i - 1] = ay[i];
+    }
+
+    // Put a new value at the end of the array
+    ax[num - 1] += random(-range, range);
+    ay[num - 1] += random(-range, range);
+
+    // Constrain all points to the screen
+    ax[num - 1] = constrain(ax[num - 1], 0, width);
+    ay[num - 1] = constrain(ay[num - 1], 0, height);
+
+    for (var j = 1; j < num; j++) {
+      ellipse(ax[j], ay[j], 24, 24);
+    }
+  }
+}
+
+function mousePressed() {
+  Object.values(points).map(function(objectValue, index) {
+    if (
+      mouseX >= parseInt(objectValue[0]) &&
+      mouseX <= parseInt(objectValue[0]) + 20
+    ) {
+      if (
+        mouseY >= parseInt(objectValue[1]) &&
+        mouseY <= parseInt(objectValue[1]) + 20
+      ) {
+        if (
+          isNaN(Object.keys(points).find(key => points[key] === objectValue))
+        ) {
+          data = [
+            "12345",
+            "28749",
+            "29573",
+            "20958",
+            "98738",
+            "46382",
+            "74847",
+            "13337",
+            "37489",
+            "57585",
+            "12095"
+          ];
+          background(150, 150, 150);
+          renderCities(data);
+        } else {
+          renderPeople();
+        }
+      }
+    }
+  });
+}
--- notD3Stuff/level1.js
+++ notD3Stuff/level1.js
@@ -9,13 +9,18 @@ function getPointClicked(ev) {
   Object.keys(points).map(function(objectKey, index) {
     if (
       x >= parseInt(objectKey.split(",")[0]) &&
-      x <= parseInt(objectKey.split(",")[0]) + 20
+      x <= parseInt(objectKey.split(",")[0]) + size
     ) {
       if (
         y >= parseInt(objectKey.split(",")[1]) &&
-        y <= parseInt(objectKey.split(",")[1]) + 20
+        y <= parseInt(objectKey.split(",")[1]) + size
       ) {
         console.log(points[objectKey]);
+        if (isNaN(points[objectKey])) {
+          renderCities(data, "#cccccc", "#666666");
+        } else {
+          renderPeople();
+        }
         return points[objectKey];
       }
     }
@@ -27,23 +32,22 @@ var ctx = canvas.getContext("2d"),
   cy = 150,
   radius = 148;
 
-function clearCanvas() {
-  ctx.fillStyle = "#0d6d0c";
+function clearCanvas(hex) {
+  ctx.fillStyle = hex;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.fill();
 }
 
-function renderCities() {
-  data = ["London", "Edinburgh", "Glasgow", "Aberdeen", "York"];
-  clearCanvas();
+function renderCities(data, back, fore) {
+  clearCanvas(back);
 
   count = data.length;
 
-  ctx.fillStyle = "#cccccc";
+  ctx.fillStyle = fore;
   while (count) {
-    var x = Math.random() * (canvas.width - 100) + 50;
-    var y = Math.random() * (canvas.height - 100) + 50;
-    ctx.fillRect(x, y, 20, 20);
+    var x = Math.random() * (canvas.width - size);
+    var y = Math.random() * (canvas.height - size);
+    ctx.fillRect(x, y, size, size);
     count--;
     var text = data[count];
     ctx.fillText(text, x - text.length, y - 2.5);
@@ -56,6 +60,60 @@ let points = {};
 
 function renderPostcodes(city) {}
 
-function renderPeople(postcode) {}
+function renderPeople() {
+  clearCanvas("#666666");
+  data = ["Person1", "Person2", "Person3", "Person4"];
+  count = data.length;
+  while (count) {
+    count--;
+  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-renderCities();
+  // Where is the circle
+  var x, y;
+
+  function setup() {
+    createCanvas(720, 400);
+    // Starts in the middle
+    x = width / 2;
+    y = height;
+  }
+
+  function draw() {
+    background(200);
+
+    // Draw a circle
+    stroke(50);
+    fill(100);
+    ellipse(x, y, 24, 24);
+
+    // Jiggling randomly on the horizontal axis
+    x = x + random(-1, 1);
+    // Moving up at a constant speed
+    y = y - 1;
+
+    // Reset to the bottom
+    if (y < 0) {
+      y = height;
+    }
+  }
+}
+
+let size = 20;
+data = ["London", "Edinburgh", "Glasgow", "Aberdeen", "York"];
+renderCities(data, "#0d6d0c", "#cccccc");
+
+size = 100;
+data = [
+  "12345",
+  "28749",
+  "29573",
+  "20958",
+  "98738",
+  "46382",
+  "74847",
+  "13337",
+  "37489",
+  "57585",
+  "12095"
+];
